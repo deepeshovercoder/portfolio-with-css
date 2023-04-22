b82d25517c53c3fa63296627af61f6eaa9d44694
Deepesh <119159930+deepeshovercoder@users.noreply.github.com>
2023-04-22 10:54:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Blogger","Programmer"],
+    typeSpeed: 100,
+    backSpeeed: 100,
+    backDelay: 1000,
+    loop: true
+})
